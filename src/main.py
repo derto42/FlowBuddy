4821c70fc21f31eab44631621f5a7fdce59ffe8c
Bimal Mallick <trisky650@gmail.com>
2023-05-17 20:09:48 +0530

    Added docstrings and formatted the code

--- src/main.py
+++ src/main.py
@@ -1,22 +1,28 @@
 import sys
-import time
-import webbrowser
-from functools import partial
 from typing import Callable
+
 
 import keyboard
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import (
-    QApplication,
-    QMenu,
-    QSystemTrayIcon,
-)
+from PyQt5.QtWidgets import QApplication, QMenu, QSystemTrayIcon
+
 
 import FileSystem as FS
 from ui import MainWindow
 
+"""It displays the title of the application"""
 
 TITLE = "FlowBuddy"
+
+"""Here in the code below  Displays the tray icon along with tooltip of the application
+    Args:
+        parent (QApplication): The parent application to which the tray icon belongs.
+        activate_action (Callable): A callable function to be triggered when the tray 
+        icon is activated.
+
+    Returns:
+        None
+"""
 
 
 def show_tray_icon(parent: QApplication, activate_action: Callable):
@@ -35,16 +41,24 @@ def show_tray_icon(parent: QApplication, activate_action: Callable):
     tray_icon.setContextMenu(menu)
 
 
+"""This trigggers the main function of the application
+    Args:
+        None
+        After the main function is executed, it runs  a series of events like 
+            updating , showing and hiding the tray icon. adding a hotkey shortcut to 
+            window"""
+
+
 def main():
     app = QApplication(sys.argv)
 
     window = MainWindow()
 
-    if any(x.lower() == '-showui' for x in app.arguments()):
+    if any(x.lower() == "-showui" for x in app.arguments()):
         QApplication.instance().processEvents()
         window.update()
         window.show()
-        
+
         # QTimer.singleShot(10, window.window_toggle_signal.emit)
 
     show_tray_icon(app, window.window_toggle_signal.emit)
