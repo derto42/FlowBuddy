6736e607082ca098721943e4d411988a6bcc2059
VasigaranAndAngel <72515046+VasigaranAndAngel@users.noreply.github.com>
2023-05-18 22:27:19 +0530

    ToolTip texts added in dialog boxes.

--- src/ui/dialog.py
+++ src/ui/dialog.py
@@ -64,6 +64,8 @@ class BaseDialog(QDialog, BaseWindow):
         button_layout.addStretch()
         accept_button.clicked.connect(lambda : self.accept())
         reject_button.clicked.connect(lambda : self.reject())
+        accept_button.setToolTip("Ok")
+        reject_button.setToolTip("Cancel")
         accept_button.setDefault(True)
         
         self.setLayout = self._main_layout.setLayout
@@ -103,6 +105,7 @@ class GroupDialog(BaseDialog):
     def for_edit(self, name: str):
         self.setTitle("Edit Group")
         self._name_entry.setText(name)
+        self._name_entry.setToolTip("Group Name")
         
         
     def result(self) -> str | QDialog.DialogCode:
@@ -129,6 +132,11 @@ class TaskDialog(BaseDialog):
         self._url_entry = Entry(self, "URL")
         file_choose_button = TextButton(self, "Choose File")
         file_choose_button.clicked.connect(self._choose_file)
+
+        self._name_entry.setToolTip("Task Name")
+        self._button_entry.setToolTip("Button Text")
+        self._url_entry.setToolTip("URL")
+        file_choose_button.setToolTip("Choose File")
 
         layout.addWidget(self._name_entry)
         layout.addWidget(self._button_entry)
