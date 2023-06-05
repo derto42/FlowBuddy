d7ea1b529ae7d7680823ffb8f7acc758eabb8dc0
VasigaranAndAngel <72515046+VasigaranAndAngel@users.noreply.github.com>
2023-06-05 17:43:34 +0530

    addon modules are now accessible from addon.py

--- src/addon.py
+++ src/addon.py
@@ -1,4 +1,6 @@
-from typing import Callable
+from __future__ import annotations
+from types import ModuleType
+from typing import Callable, overload
 from importlib import import_module
 from glob import glob
 from os.path import split
@@ -10,15 +12,31 @@ from FileSystem import exists, ADDONS_FOLDER, ADDONS_NAME
 from utils import HotKeys
 
 
+add_ons: dict[str, ModuleType] = {}
+
+currently_loading_module = None
+
+
 class AddOnBase:
     system_tray_icon: QSystemTrayIcon = None # instance of QSystemTrayIcon will be assigned after initializing it
+    instences: dict[str, AddOnBase] = {}
+    
+    def __init__(self):
+        AddOnBase.instences[currently_loading_module] = self
+        
+    def activate(self):
+        """Override this method to call when desktop widget is activated."""
+        pass
+    
     def set_shortcut(self, shortcut: str, function: Callable) -> None:
+        """Adds a global shortcut"""
         HotKeys.add_global_shortcut(shortcut, function)
     
 
 
 def load_addons() -> None:
     """Loads all the modules from the ADDONs folder."""
+    global add_ons, currently_loading_module
     if exists(ADDONS_FOLDER):
         # traverse root directory, and list directories as dirs and files as files
         for root, dirs, files in os.walk(ADDONS_FOLDER):
@@ -27,5 +45,8 @@ def load_addons() -> None:
                 file_path = os.path.join(dir_path, f"{dir}.py")
                 if os.path.isfile(file_path):  # If the .py file with same name as directory exists
                     # Import the module
-                    import_module(f'{ADDONS_NAME}.{dir}.{dir}')
-            
+                    module_name = f'{ADDONS_NAME}.{dir}.{dir}'
+                    currently_loading_module = module_name
+                    module = import_module(module_name)
+                    currently_loading_module = None
+                    add_ons[module_name] = module
