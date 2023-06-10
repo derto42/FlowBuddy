cc6d1e4ce0d79c75bc939419603a3274e4ee841f
VasigaranAndAngel <72515046+VasigaranAndAngel@users.noreply.github.com>
2023-06-10 21:48:37 +0530

    order of loading addons can be configured.

--- src/addon.py
+++ src/addon.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import json
 from types import ModuleType
 from typing import Callable, Optional
 from importlib import import_module
@@ -20,20 +21,93 @@ currently_loading_module = None
 def load_addons() -> None:
     """Loads all the modules from the ADDONs folder."""
     global add_ons, add_on_paths, currently_loading_module
+    
+    def apply_order(modules_and_paths: dict[str, str]) -> dict[str, str]:
+        order_file = f"{ADDONS_FOLDER}/order.json"
+        if not exists(order_file):
+            default_order_data = {
+                    "_comment": [
+                        "High priority addons. These addons will be loaded first.",
+                        "Those addons which not specified will be considered as 'medium_priority' and will be loaded after loading hight_priority addons.",
+                        "Low priority addons. These addons will be loaded after loading all the addons.",
+                        "Names are case insensitive."
+                    ],
+
+
+                    "high_priority": [
+                        "shortcuts",
+                        "notes",
+                        "youtube_downloader"
+                    ],
+
+                    "medium_priority": [],
+
+                    "low_priority": [
+                        "settings"
+                    ]
+                }
+            with open(order_file, "w") as f:
+                json.dump(default_order_data, f, indent=4)
+
+        else:
+            try:
+                # apply sorting if possible.
+                if exists(order_file):
+                    with open(order_file, "r") as f:
+                        order_data = json.load(f)
+                        
+                    high_priorities   = [x.lower() for x in order_data["high_priority"]]
+                    medium_priorities = [x.lower() for x in order_data["medium_priority"]]
+                    low_priorities    = [x.lower() for x in order_data["low_priority"]]
+                    
+                    modules_name_in_lower = {x.split(".")[-1].lower(): x for x in modules_and_paths}
+                    
+                    priority_addons = []
+                    for priority in [high_priorities, medium_priorities, low_priorities]:
+                        addons = {}
+                        for module in priority:
+                            if module in modules_name_in_lower:
+                                module_name = modules_name_in_lower[module]
+                                addons[module_name] = modules_and_paths.pop(module_name)
+                        priority_addons.append(addons)
+                        
+                    high_priority_addons, medium_priority_addons, low_priority_addons = priority_addons
+
+                    rest_addons = modules_and_paths
+
+                    return {**high_priority_addons,
+                            **medium_priority_addons,
+                            **rest_addons,
+                            **low_priority_addons}
+
+            except Exception as e:
+                print(f"Error occurred while applying order for addons.\n{e}")
+            
+        return modules_and_paths
+    
+    
     if exists(ADDONS_FOLDER):
         # traverse root directory, and list directories as dirs and files as files
+        modules_and_paths = {}
+        
         for root, dirs, files in os.walk(ADDONS_FOLDER):
-            for dir in dirs:
-                dir_path = os.path.join(root, dir)
-                file_path = os.path.join(dir_path, f"{dir}.py")
+            for name in dirs:
+                dir_path = os.path.join(root, name)
+                file_path = os.path.join(dir_path, f"{name}.py")
                 if os.path.isfile(file_path):  # If the .py file with same name as directory exists
-                    # Import the module
-                    module_name = f'{ADDONS_NAME}.{dir}.{dir}'
-                    currently_loading_module = module_name
-                    module = import_module(module_name)
-                    currently_loading_module = None
-                    add_ons[module_name] = module
-                    add_on_paths[module_name] = file_path
+                    module_name = f'{ADDONS_NAME}.{name}.{name}'
+                    modules_and_paths[module_name] = file_path
+        
+        modules_and_paths = apply_order(modules_and_paths)
+        
+        for module_name in modules_and_paths:
+            # Import the module
+            currently_loading_module = module_name
+            module = import_module(module_name)
+            currently_loading_module = None
+            add_ons[module_name] = module
+            add_on_paths[module_name] = modules_and_paths[module_name]
+
 
 
 class AddOnBase:
