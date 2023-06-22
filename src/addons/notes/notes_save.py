e5dceece425cbd4c14da756027f128ddd157c92e
Lee Kadin <lkadin@gmail.com>
2023-06-22 10:56:34 -0400

    add create_config_from_text_files

--- src/addons/notes/notes_save.py
+++ src/addons/notes/notes_save.py
@@ -11,15 +11,6 @@ PLATFORM = sys.platform
 CONFIG_FILE = os.path.join(ADDONS_FOLDER, "config.json")
 
 
-# If there are any exceptions related to JSON decoding, file not found, or missing keys,
-# a save file is created using the FS module.
-try:
-    with open(CONFIG_FILE) as f:
-        data = json.load(f)
-    _, _ = data["files"], data["last_active"]
-except (json.JSONDecodeError, FileNotFoundError, KeyError):
-    with open(CONFIG_FILE, 'w') as f:
-        f.write(json.dumps ({"files": [], "last_active": 0}))
 
 
 
@@ -51,3 +42,22 @@ def open_file(file_path: str | None) -> None:
             os.startfile(file_path)
         elif PLATFORM in ("linux", "darwin"):
             os.system(f"xdg-open {file_path}")
+
+def create_config_from_text_files():
+        config = {
+            "files": [
+                file_name
+                for file_name in os.listdir(ADDONS_FOLDER)
+            ],
+            "last_active": 0,
+        }
+        with open(CONFIG_FILE, "w") as f:
+            f.write(json.dumps (config))
+# If there are any exceptions related to JSON decoding, file not found, or missing keys,
+# a save file is created using the FS module.
+try:
+    with open(CONFIG_FILE) as f:
+        data = json.load(f)
+    _, _ = data["files"], data["last_active"]
+except (json.JSONDecodeError, FileNotFoundError, KeyError):
+    create_config_from_text_files()
